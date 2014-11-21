14dbe42d447d9298c15e7b30654bc3666b88bea1
BruceJohnJennerLawso <johnnybgoode@rogers.com>
2014-11-21 01:48:02 -0500

    added the input functions, the integer one has a weird but non critical bug

--- ctest.cpp
+++ ctest.cpp
@@ -9,5 +9,57 @@
 
 //#include "Headers.hpp"
 //#include "Source.cpp"
+#include "VectorVictor2.hpp"
+#include <iostream>
+#include <string>
+
+typedef unsigned int integer;
+
+integer inputInteger(std::string prompt);
+
+std::string inputString(std::string prompt);
 
 
+int main()
+{	integer hello = -1;
+	hello = inputInteger("Please Input Integer Number: ");
+	std::cout << hello << std::endl;
+	return 0;
+}
+
+
+integer inputInteger(std::string prompt)
+{
+	while(true)
+	{	std::cout << prompt;
+		integer data;
+		std::cin >> data;
+		if(!std::cin.fail())
+		{	return data;
+		}
+		else
+		{	while(std::cin.fail())
+			{	std::cin.clear();
+			}
+		}
+		std::cin.ignore();
+		std::cout << "\n" << std::endl;
+	}
+}
+
+
+std::string Input_string(std::string prompt)
+{
+	std::string data = "";
+	while(true)
+	{	std::cout << prompt;
+		while(std::getline(std::cin, data))
+		{	if(data.size() == 0)
+			{	continue;
+			}
+			else
+			{	return data;
+			}
+		}
+	}
+}
