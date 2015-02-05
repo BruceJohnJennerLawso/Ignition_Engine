c5cb9bed2eafe9d010124af470dd488a0373bfb3
bruce <johnnybgoode@rogers.com>
2015-02-05 17:16:51 -0500

    found an issue in the VV2 where a rotated vector gradually spirals inward with increasing rotation more testing to follow to determine the cause

--- bobby.cpp
+++ bobby.cpp
@@ -16,29 +16,51 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(2);
-    shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
+	sf::CircleShape shape(2);
+	shape.setFillColor(sf::Color::Green);
     
-    VectorVictor::Vector2 fuubar(10, -10);
+	VectorVictor::Vector2 fuubar(10, -10);
     
-    sf::CircleShape vector(2);
-    vector.setFillColor(sf::Color::Blue);
-    vector.setPosition(sf::Vector2f(fuubar.x, -fuubar.y));
+	sf::CircleShape vector(2);
+	vector.setFillColor(sf::Color::Blue);
+	vector.setPosition(sf::Vector2f(fuubar.x, -fuubar.y));
     
-    
+	sf::Font font;
+	if (!font.loadFromFile("./Data/Fonts/orbitron-light.ttf"))
+	{	// I dont care right now
+	}
+	sf::Text text;
+	text.setFont(font);
+	text.setPosition(sf::Vector2f(0, 470));
+	
 
-    while (window.isOpen())
-    {	sf::Event event;
+	while(window.isOpen())
+	{	sf::Event event;
 		while (window.pollEvent(event))
-		{	if (event.type == sf::Event::Closed)
-            {	window.close();
+		{	if(event.type == sf::Event::Closed)
+			{	window.close();
+			}
+			if(event.type == sf::Event::MouseButtonPressed)
+			{	if(event.mouseButton.button == sf::Mouse::Button::Left)
+				{	sf::Vector2i input_click = sf::Mouse::getPosition(window);
+					fuubar.x = input_click.x;
+					fuubar.y = -input_click.y;	
+				}
+				if(event.mouseButton.button == sf::Mouse::Button::Right)
+				{	fuubar.Rotate_vector(30);
+				}
 			}
 		}
+	    vector.setPosition(sf::Vector2f(fuubar.x, -fuubar.y));
+	    text.setString(SI::Get_formatted_value(fuubar.Get_vector_magnitude() , 3, "units"));
+	    
 		window.clear();
 		window.draw(shape);
 		window.draw(vector);
+		window.draw(text);
+		
 		window.display();
     }
-    return 0;
+	return 0;
 }
