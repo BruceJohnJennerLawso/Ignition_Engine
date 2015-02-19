eb4085a8a59f8fd8eb2b99f86100be2f19e4e32e
bruce <johnnybgoode@rogers.com>
2015-02-19 15:50:49 -0500

    fleshed out the MFD class, looks quite workable, just want to look at the Orbiter MFDs for inspriration before I actually do anything with it

--- mfd.cpp
+++ mfd.cpp
@@ -6,3 +6,99 @@
 //#include "Headers.hpp"
 //#include "Source.cpp"
 #include "mfd.hpp"
+
+
+bool MFD::Render_MFD(SFML_Window &window, key_commands &keyCommands, Cursor_commands &cursorCommands, long double &cameraScale, long double dt, 	int &time_acceleration, long double sim_time, TVessel &current_vessel, std::vector<CNewtonian_Object*> &newtonians, std::vector<CKeplerian_Object*> &keplerians, std::vector<TVessel*> &vessels, VectorVictor::Vector2 &Camera_target, long double &Camera_rotation)
+{	Talkback("Bad call to MFD::Render_MFD(SFML_Window &window, key_commands &keyCommands, Cursor_commands &cursorCommands, long double &cameraScale, long double dt, 	int &time_acceleration, long double sim_time, TVessel &current_vessel, std::vector<CNewtonian_Object*> &newtonians, std::vector<CKeplerian_Object*> &keplerians, std::vector<TVessel*> &vessels, VectorVictor::Vector2 &Camera_target, long double &Camera_rotation)");
+}
+
+void MFD::Draw_MFD(SFML_Window &window, sf::Color Placard_color)
+{	// okay, here we go
+	sf::RectangleShape placard(Height, Width);
+	placard.setColor(Placard_color);
+	// we set the background to the mfd to the passed colour
+	placard.setPosition(this->Get_mfd_position(window));
+	// and we set its position in the window appropriately
+	window.window->draw(placard);
+	// and draw it to the window
+	
+	
+	// now we need to do the same with whatever the MFD spat out
+	sf::Sprite mfd_sprite(MFD_image);
+	// create a sprite that uses the texture we generated for the mfd
+	mfd_sprite.setPosition(this->Get_mfd_position(window));
+	// set its position in the window, same as with the placard background
+	window.window->draw(mfd_sprite);
+	// and draw it onscreen
+}
+
+sf::Vector2f MFD::Get_mfd_position(SFML_Window &window)
+{	sf::Vector2f position();
+	if(MFD_orientation == upper_left)
+	{	// the simple case, we do as usual
+		position = MFD_offset;
+		// we set it equal to our offset, easy mode
+		return position;
+	}
+	else if(MFD_orientation == upper_right)
+	{	//a wee bit more complex, this time our y axis is normal, but the x axis
+		// gets interesting
+		float deltax = (float)window.Width - (MFD_offset.x + Width);
+		position.x = deltax;
+		position.y = MFD_offset.y;
+		return position;
+	}
+	else if(MFD_orientation == lower_left)
+	{	// similar situation, 'cept now the y axis is reversed
+		float deltay = (float)window.Height - (MFD_offset.y + Height);
+		position.y = deltay;
+		position.x = MFD_offset.x;
+		return position;
+	}
+	else if(MFD_orientation == lower_right)
+	{	// the last case, stuck to the far bottom side of the window
+		float deltax = (float)window.Width - (MFD_offset.x + Width);
+		float deltay = (float)window.Height - (MFD_offset.y + Height);
+		position.x = deltax;
+		position.y = deltay;
+		return position;
+	}
+}
+
+
+bool Surface_MFD::Render_MFD(SFML_Window &window, key_commands &keyCommands, Cursor_commands &cursorCommands, long double &cameraScale, long double dt, 	int &time_acceleration, long double sim_time, TVessel &current_vessel, std::vector<CNewtonian_Object*> &newtonians, std::vector<CKeplerian_Object*> &keplerians, std::vector<TVessel*> &vessels, VectorVictor::Vector2 &Camera_target, long double &Camera_rotation)
+{
+	sf::RenderTexture canvas;
+	// we create a special sfml object that allows us to draw to it just like
+	// it is a screen, even though its not. Then at the end we can retrieve a
+	// texture from it and use that for drawing
+	if(!canvas.create(Height, Width))
+	{	// something screwed up, maybe we got no more memory to play with
+		Talkback("Unable to Create MFD canvas");
+		return false;
+	}	// also need to check to see whether the order of height/width is
+		// correct
+	canvas.clear();
+	
+	// we set up any old drawable and go
+	
+	// canvas.draw(stuff);
+	
+	// on the one hand, I could always just write another function that takes
+	// our crapload of arguments like before along with the rendertexture canvas
+	// by reference and draws it
+	
+	// but I dont wanna make things more complex just to be idiotproof, so
+	// if you are an idiot, GTFO!!!
+	
+	// If you are new to making mfds and want to learn, just read the SFML
+	// tutorial docs on the sfml website regarding graphics and windows and
+	// you should have no problem understanding this
+	
+	canvas.display();
+	// not a hundred percent sure what this does, but I think it is necessary
+	
+	MFD_image = canvas.getTexture();
+	return true;
+	// we done here, everything went okay
+}
--- mfd.hpp
+++ mfd.hpp
@@ -11,18 +11,50 @@ enum orientation{upper_left, lower_left, upper_right, lower_right};
 
 class MFD
 {	public:
-	virtual void Render_MFD(SFML_Window &window, key_commands &keyCommands, Cursor_commands &cursorCommands, long double cameraScale, long double dt, 	int time_acceleration, long double sim_time, TVessel &current_vessel, std::vector<CNewtonian_Object*> &newtonians, std::vector<CKeplerian_Object*> &keplerians, std::vector<TVessel*> &vessels, VectorVictor::Vector2 &Camera_target, long double &Camera_rotation);
+	virtual bool Render_MFD(SFML_Window &window, key_commands &keyCommands, Cursor_commands &cursorCommands, long double &cameraScale, long double dt, 	int &time_acceleration, long double sim_time, TVessel &current_vessel, std::vector<CNewtonian_Object*> &newtonians, std::vector<CKeplerian_Object*> &keplerians, std::vector<TVessel*> &vessels, VectorVictor::Vector2 &Camera_target, long double &Camera_rotation);
 	// phew, I think that is everything
-	virtual void Render_MFD(SFML_Window &window);
-	// and a second version that we can call between update passes, since we
-	// wanna sync mfd update calls with updating the standard displays too, so
-	// we dont have stuff redrawing itself way too fast every frame
+	void Draw_MFD(SFML_Window &window, sf::Color Placard_color);
+//	virtual void Draw_MFD(SFML_Window &window, sf::Texture Placard_background, bool scale_lock);	
+	// and the function that actually creates a sprite and slaps it onto the 
+	// SFML window that we pass by reference
+	
+	// we call Render_MFD to refresh the image every couple of milliseconds, and
+	// we call Draw_MFD to actually draw the MFD to the window *EVERY FRAME*
+	// sudo dodododo important, must remember that
+	
+	// also, each vessel calls Draw MFD in a function that Ignition Engine
+	// calls for it, so that we can pass internal parameters like the colour for
+	// each vessel
+	
 	sf::Texture MFD_image;
-	orientation Image_orientation;
-	sf::Vector2f Image_offset;
+	orientation MFD_orientation;
+	
+	sf::Vector2f MFD_offset;
 	// ie this way we can tell it where it should be relative to onscreen, ie
 	// from which corner, so that mfds show up in the right places regardless
 	// of window size
+	float Height, Width;
+	// define two things:
+	// -the max size of the Texture image output size (if we go over it, we can
+	// just relative scale the output sprite back to fit in the correct size)
+	
+	// the size of the 'placard' that we draw behind the mfd so it has a
+	// background to make it easier to see
+	// this is an sf::Color stored by the vessel, so individual vessels can have
+	// a 'style'
+	
+	// in a future version commented out for the moment, it can also be done as
+	// a passed texture which can be scale locked, so that users can create cool
+	// looking mfd styles (like an apollo panel for the Apollo CSM MFDs)
+	// this might be a wee bit glitchy for the mfd having its image forcibly
+	// resized like that, but the feature is worthwhile IMO
+	
+	sf::Vector2f Get_mfd_position(SFML_Window &window);
+	// uses the render window, our image offset, the height, the width, and the
+	// orientation to tell us where to draw stuff
+	
+	// specifically, this tells us in window coordinates, the location of
+	// the upper left corner of this MFD
 	
 	//virtual bool Load(filestuffz);
 	//virtual bool Save(filestuffz&);
@@ -42,6 +74,11 @@ class Surface_MFD: public MFD
 	
 	// also the window orientation... etc.
 	
-	void Render_MFD(SFML_Window &window, key_commands &keyCommands, Cursor_commands &cursorCommands, long double cameraScale, long double dt, 	int time_acceleration, long double sim_time, TVessel &current_vessel, std::vector<CNewtonian_Object*> &newtonians, std::vector<CKeplerian_Object*> &keplerians, std::vector<TVessel*> &vessels, VectorVictor::Vector2 &Camera_target, long double &Camera_rotation);
-	void Render_MFD(SFML_Window &window);
+	bool Render_MFD(SFML_Window &window, key_commands &keyCommands, Cursor_commands &cursorCommands, long double &cameraScale, long double dt, int &time_acceleration, long double sim_time, TVessel &current_vessel, std::vector<CNewtonian_Object*> &newtonians, std::vector<CKeplerian_Object*> &keplerians, std::vector<TVessel*> &vessels, VectorVictor::Vector2 &Camera_target, long double &Camera_rotation);
+	//void Draw_MFD(SFML_Window &window, sf::Color Placard_color);
+	// dont actually need the function, its done in the MFD class since we dont
+	// need to know anything about the specific mfd to do the grunt work
+	// of drawing it
+	
+	~Surface_MFD();
 };
