bf6b8948ada885f74a10d94f362e901a121d5851
bruce <johnnybgoode@rogers.com>
2015-09-02 16:23:17 -0400

    added run and vector test bash scripts

--- gopy/ignition_engine.py
+++ gopy/ignition_engine.py
@@ -5,6 +5,22 @@ from vessel import *
 import sys
 
 
+class Ignition_Camera:
+	def __init__(self, position, rotation):
+		self.Position = position
+		self.Rotation = rotation
+		
+	def getCameraPosition(self):
+		return self.Position
+	
+	def getCameraRotation(self):
+		return self.Rotation
+		
+	def Update(self, deltat, target, window_height, window_width):
+		self.Rotation = target.getRotation()
+		offset = vector_II((-window_width),(window_height)).rotateThis(self.Rotation)
+		self.Position = (target.getPosition() + offset )
+		return
 
 class Ignition_Engine:
 	def __init__(self, redraw_interval, window_title, window_height, window_width):
@@ -17,26 +33,23 @@ class Ignition_Engine:
 		self.windowHeight = window_height
 		self.windowWidth = window_width
 		
+		self.mainCamera = Ignition_Camera(vector_II(0,0), 0.0)
 		
 		self.Vessels = []
 		self.Vessels.insert(0, Vessel( vector_II(100, 20), vector_II(2, 0), 22, 0, "Almighty Probe", "almighty.jpg"))
 	
+		self.cameraTarget = self.Vessels[0];
+		
 	def Ignition(self):
 		pygame.init()
 		size = width, height = (self.windowWidth, self.windowHeight)
-		speed = [1, 1]
 		black = 0, 0, 0
-		
 		screen = pygame.display.set_mode(size)
-		
-		ball = pygame.image.load("almighty.jpg")
-		
 		clock = pygame.time.Clock()
-		
-		ballrect = ball.get_rect()
-		print ballrect
 		while 1:
+			clock.tick()
 			deltat = clock.get_time()
+			print "Deltat is %f" % deltat
 			clock.tick(60)
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT: 
@@ -47,19 +60,11 @@ class Ignition_Engine:
 			
 			screen.fill(black)
 			
-			for v in self.Vessels:
-				print
+			self.mainCamera.Update(deltat, self.cameraTarget, self.windowHeight, self.windowWidth)
+			
+			for v in self.Vessels:				
 				drawPos = (int(v.getPosition().x), int(v.getPosition().y), 154, 88)
-				print drawPos
-				#screen.blit(ball, drawPos )
 				screen.blit(v.getImage(), drawPos )				
-				
-			#ballrect = ballrect.move(speed)
-			#if ballrect.left < 0 or ballrect.right > width:
-			#	speed[0] = -speed[0]
-			#if ballrect.top < 0 or ballrect.bottom > height:
-			#	speed[1] = -speed[1]
-			#screen.blit(ball, ballrect)
 			pygame.display.flip()
 
 if (__name__=="__main__"):
--- gopy/vector_X.py
+++ gopy/vector_X.py
@@ -1,6 +1,16 @@
 ## vector.py ###################################################################
 ## vector3 class, first module of the project ##################################
 ################################################################################
+import math
+
+
+def degreesToRadians(degrees):
+	output = ((degrees*2*math.pi)/360)
+	return output
+	
+def radiansToDegrees(radians):
+	output = ((radians*360)/(2*math.pi))
+	return output
 
 
 class vector_II:
@@ -42,6 +52,26 @@ class vector_II:
 		output = ((self.x*vec.x)+(self.y*vec.y));
 		return output;
 		
+	def Rotate(self, rotation):
+		""" Return a rotated version of this vector, rotated
+		by rotation radians clockwise. Does not modify the
+		original vector """
+		output = vector_II(0,0)
+		
+		output.x = ( (self.x*(math.cos(rotation)))+(self.y*(math.sin(rotation))) )
+		output.y = ( (self.x*((-1)*math.sin(rotation)))+(self.y*(math.cos(rotation))) )
+		
+		return output
+		
+		
+	def rotateThis(self, rotation):
+		""" Rotate this vector by rotation radians clockwise.
+		Does modify the original vector """
+		new_x = ( (self.x*(math.cos(rotation)))+(self.y*(math.sin(rotation))) )
+		self.y = ( (self.x*((-1)*math.sin(rotation)))+(self.y*(math.cos(rotation))) )
+		self.x = new_x
+		return
+		
 	def This(self):
 		return self;
 		# always unto thyself return?
@@ -50,7 +80,7 @@ if (__name__=="__main__"):
 	v = vector_II(2, 2)
 	w = vector_II(1, -1)
 	print "v: %sw: %s" % (v.getVec(), w.getVec())
-	z = v + w
+	z = v + w.This()
 	print "v: %sw: %sz: %s" % (v.getVec(), w.getVec(), z.getVec())
 	
 	z = v*3
